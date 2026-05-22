395e32646515843858b07b15564f4c9cdb2876b7
aceburnout0 <aceburnout0@gmail.com>
2026-05-22 15:44:33 -0300

    u1

---
+++ script.js
@@ -0,0 +1,13 @@
+function dango1() {
+	alert('Daniel Almeida')
+}
+
+function dango2() {
+	alert('Daniel da Silva')
+}
+
+function nomeEx() {
+	let nome = document.getElementById('inputN').value
+
+	alert('Bem-vindo(a) ' + nome + '!')
+}
